f491f49343d5a390969c00d7c3d065917bad1cb6
SAKTHIIIVEL <sssakthivel928@gmail.com>
2025-03-15 13:26:37 +0530

    new changes

--- app/src/main/java/com/example/ecommerce_project/activities/HomepageActivity.java
+++ app/src/main/java/com/example/ecommerce_project/activities/HomepageActivity.java
@@ -1,8 +1,12 @@
 package com.example.ecommerce_project.activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -14,12 +18,16 @@ import android.view.MenuItem;
 
 import com.example.ecommerce_project.R;
 import com.example.ecommerce_project.fragments.HomeFragment;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class HomepageActivity extends AppCompatActivity {
+public class HomepageActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     Toolbar toolbar;
     FirebaseAuth auth;
+
+    DrawerLayout drawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +38,17 @@ public class HomepageActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.Home_toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.baseline_menu_24);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle =  new ActionBarDrawerToggle(this,drawerLayout, toolbar,R.string.open_nav,R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
 
         if (savedInstanceState == null) { // Prevents fragment reloading on screen rotation
             loadFragment(new HomeFragment());
+            navigationView.setCheckedItem(R.id.nav_home);
         }
     }
 
@@ -65,6 +78,29 @@ public class HomepageActivity extends AppCompatActivity {
         } else if (id == R.id.menu_my_cart) {
             startActivity(new Intent(HomepageActivity.this, CartActivity.class));
         }
+        return true;
+    }
+
+    //ithula nav button click action podu
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.nav_home) {
+            Intent intent = new Intent(this, HomepageActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_cart) {
+            Intent intent = new Intent(this, CartActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.All_Prod) {
+            Intent intent = new Intent(this, ShowAllActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_payment) {
+            Intent intent = new Intent(this, PaymentActivity.class);
+            startActivity(intent);
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 }
